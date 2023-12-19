db9c1f1b1b3f4e33258a7506dcf6af98073f143b
Christian Santana <cs632706@gmail.com>
2023-12-19 11:33:33 -0600

    ...

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from pyrogram import Client
+
+api_id = 25566003
+api_hash = "f36cd30cf88e8a3030cc9b714e75c932"
+bot_token = "6973403026:AAFCcqRrZCnCmDs997kQwzzzAKZQPTpdsK0"
 plugins = dict(root="plugins")
 
-app = Client("my_account", plugins=plugins)
+app = Client("my_account", plugins=plugins, api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 app.run()
