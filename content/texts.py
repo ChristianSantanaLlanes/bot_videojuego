1cddf8c21ed97d2d2770a5bb3d5d45adfb5a678f
wilouuu <121647976+wilouuu@users.noreply.github.com>
2023-12-23 23:21:35 -0500

    Update texts.py

--- content/texts.py
+++ content/texts.py
@@ -2,7 +2,7 @@ TEXTS = {
     'start_text': '''
 ¡Hola {}! Bienvenido al bot de videojuegos. Aquí podrás encontrar información sobre tus juegos favoritos, como la foto, el nombre, la descripción y mucho más.
 
-Para empezar, envíame el nombre del juego que quieres buscar. Por ejemplo, si quieres buscar información sobre "Elden Ring", envíame "Elden Ring".
+Para empezar, usa la opción "Buscar Videojuego" y envíame el nombre del juego que quieres buscar. Por ejemplo, si quieres buscar información sobre "Elden Ring", envíame "Elden Ring".
 
 También puedes utilizar los siguientes comandos:
 
